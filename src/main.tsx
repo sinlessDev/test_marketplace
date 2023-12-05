3749c67fe439d1f37545d056b5e928ff1f58e21a
Almaz <Iheardthatwas@gmail.com>
2023-12-05 18:33:11 +0600

    feat trying to setup redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Store from "./store.tsx";
+import Products from "./products.tsx";
 import Product from "./product.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}></Route>
-      <Route path="/store" element={<Store />}></Route>
+      <Route path="/store" element={<Products />}></Route>
       <Route path="/store/:id" element={<Product />} />
     </Routes>
   </BrowserRouter>
