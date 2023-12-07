b8119a3e72f8a8afcde850e2a6211065a21cd827
Almaz <Iheardthatwas@gmail.com>
2023-12-07 16:27:56 +0600

    final submit / mobile responsivness

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -19,7 +19,7 @@ export default function Signin() {
   };
 
   return (
-    <div className="flex h-screen flex-1">
+    <div className="flex h-[100vh] min-h-screen flex-1">
       <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -32,7 +32,7 @@ const Register = () => {
 
   return (
     <>
-      <div className="flex h-screen flex-1">
+      <div className="flex h-[100vh] min-h-screen flex-1">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
